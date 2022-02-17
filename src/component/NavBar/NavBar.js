4fc8af4327b31ac06588f7af6f40c65ce572ca27
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-02-17 15:32:16 -0800

    added social icons for nav bar

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './NavBar.scss';
+import emailLogo from '../../assets/Icons/blackEmail.png';
+import linkedinLogo from '../../assets/Icons/linkedinBlack.png';
+import gitLogo from '../../assets/Icons/gitBlack.png';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
@@ -7,7 +11,14 @@ export default function NavBar() {
         <a className="nav__links" href="/about">About</a>
         <a className="nav__links" href="/skills">Skills</a>
         <a className="nav__links" href="/projects">Projects</a>
-        <a className="nav__links" href="/contact">Contact</a>
+        <div className='nav__links nav__links--social-icons'>
+          {/* <img className='social-icon' src={gmailLogo} alt="" /> */}
+          <a href='https://www.github.com/brynfrayne'><img  className='social-icon'src={gitLogo} alt="" /></a>
+          <a href="mailto:brynfrayne@gmail.com"><img  className='social-icon'src={emailLogo} alt="" /></a>
+          <a href="https://www.linkedin.com/in/brynfrayne"><img  className='social-icon'src={linkedinLogo} alt="" /></a>
+
+        </div>
+        {/* <a className="nav__links" href="/contact">Contact</a> */}
     </nav>
   )
 }
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -9,10 +9,10 @@ export default function Contact() {
   return (
     <div className='contact'>
         <div>
-            <NavLink to='/www.github.com/brynfrayne'><img src={githubLogo} alt="" />/in/brynfrayne</NavLink>
+            <NavLink to='/www.github.com/brynfrayne'><img src={githubLogo} alt="" />"/brynfrayne</NavLink>
         </div>
         <div>
-            <img src={gmailLogo} alt="" />
+            
         </div>
         <div>
             <img src={linkedinLogo} alt="" />
