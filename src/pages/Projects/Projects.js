27fa9447e0739449f497cab9dcb370e800f6e9ef
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-02-17 23:19:12 -0800

    added  responsive styling to about page & projects page

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import InvestLikeTheBest from '../../component/InvestLikeTheBest/InvestLikeTheBest';
 import InStock from '../../component/InStock/InStock';
+import './Projects.scss';
 
 export default function Portfolio() {
   return (
-    <div>
-    <InvestLikeTheBest />
-    <InStock/> 
+    <div className='projects'>
+    <InvestLikeTheBest className='project-card'/>
+    <InStock className='project-card'/> 
         
     </div>
   )
